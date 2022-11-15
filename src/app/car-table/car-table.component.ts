75e665d0455b3817b5127674ba01b23dfe1218df
/ohnsonWTC <johnsondubula@gmail.com>
2022-11-15 16:35:13 +0200

    car list component

--- src/app/car-table/car-table.component.ts
+++ src/app/car-table/car-table.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { car } from '../car';
 
 @Component({
   selector: 'app-car-table',
@@ -9,6 +10,7 @@ export class CarTableComponent implements OnInit {
 
   constructor() { }
 
+  @Input() carsList : car[] = []
   ngOnInit(): void {
   }
 
